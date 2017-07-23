940b8f7a6f4093e64edbb1795c419e518fd92910
goule1993 <875195443@qq.com>
2017-07-23 23:22:46 +0800

    核心功能：redis作为mybatis的二级缓存 作为缓存来使用，弊端，每次更新需要clear。

---
+++ src/main/java/com/example/demo/config/RedisCache.java
@@ -0,0 +1,100 @@
+package com.example.demo.config;
+
+import org.apache.ibatis.cache.Cache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Created by niewenlong-work on 2017/7/21.
+ */
+public class RedisCache implements Cache {
+    private static final Logger logger = LoggerFactory.getLogger(RedisCache.class);
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final String id; // cache instance id
+    private RedisTemplate redisTemplate;
+    private static final long EXPIRE_TIME_IN_MINUTES = 30; // redis过期时间
+    public RedisCache(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Cache instances require an ID");
+        }
+        this.id = id;
+    }
+    @Override
+    public String getId() {
+        return id;
+    }
+    /**
+     * 向redis缓存中存放数据
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public void putObject(Object key, Object value) {
+        RedisTemplate redisTemplate = getRedisTemplate();
+        ValueOperations opsForValue = redisTemplate.opsForValue();
+        opsForValue.set(key, value, EXPIRE_TIME_IN_MINUTES, TimeUnit.MINUTES);
+        logger.debug("向redis缓存中存放数据");
+    }
+    /**
+     * 从redis获取数据
+     *
+     * @param key
+     * @return
+     */
+    @Override
+    public Object getObject(Object key) {
+        RedisTemplate redisTemplate = getRedisTemplate();
+        ValueOperations opsForValue = redisTemplate.opsForValue();
+        logger.debug("从redis缓存中获取数据");
+        return opsForValue.get(key);
+    }
+    /**
+     * 从redis缓存中删除数据
+     *
+     * @param key
+     * @return
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object removeObject(Object key) {
+        RedisTemplate redisTemplate = getRedisTemplate();
+        redisTemplate.delete(key);
+        logger.debug("从redis缓存中删除数据");
+        return null;
+    }
+    /**
+     * 清空缓存
+     */
+    @Override
+    public void clear() {
+        RedisTemplate redisTemplate = getRedisTemplate();
+        redisTemplate.execute((RedisCallback) connection -> {
+            connection.flushDb();
+            return null;
+        });
+        logger.debug("清空缓存");
+    }
+    @Override
+    public int getSize() {
+        return 0;
+    }
+    @Override
+    public ReadWriteLock getReadWriteLock() {
+        return readWriteLock;
+    }
+    private RedisTemplate getRedisTemplate() {
+        if (redisTemplate == null) {
+            redisTemplate = ApplicationContextHolder.getBean("redisTemplate");
+        }
+        return redisTemplate;
+    }
+}
